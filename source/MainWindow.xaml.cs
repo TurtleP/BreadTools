1ec5b046bbc0c3dc3eacba1e8b9e0a97f06aa189
TurtleP <jpostelnek@outlook.com>
2020-09-01 10:54:01 -0400

    move to csharp

---
+++ source/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Security.Principal;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Linq;
+using Bread_Tools.Resources.Pages;
+
+namespace Bread_Tools
+{
+    public partial class MainWindow : Window
+    {
+        private GeneralPage general;
+        private CommandPage command;
+        private PowerPage power;
+        private PCSettingsPage pcSettings;
+
+        private string highLightColor = "#CFCFCF";
+        private string unHighLightColor = "#E6E6E6";
+
+        public static bool IsAdministrator()
+        {
+            using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+            {
+                WindowsPrincipal principal = new WindowsPrincipal(identity);
+                return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.general = new GeneralPage();
+            this.command = new CommandPage();
+
+            this.power = new PowerPage();
+            this.pcSettings = new PCSettingsPage();
+
+            StackPanel which = this.MainPanel.Children.OfType<StackPanel>().First();
+            this.ShowGeneralPage(which, null);
+        }
+
+        private SolidColorBrush GetBrushColor(string color)
+        {
+            SolidColorBrush brush = (SolidColorBrush)(new BrushConverter().ConvertFrom(color));
+            return brush;
+        }
+
+        private void HoverPanel(object sender, EventArgs e)
+        {
+            StackPanel panel = sender as StackPanel;
+            panel.Background = this.GetBrushColor(this.highLightColor);
+
+            this.Cursor = Cursors.Hand;
+        }
+
+        private void UnHoverPanel(object sender, EventArgs e)
+        {
+            StackPanel panel = sender as StackPanel;
+            panel.Background = this.GetBrushColor(this.unHighLightColor);
+
+            this.Cursor = Cursors.Arrow;
+        }
+
+        private void DeSelectPanelItem(object sender)
+        {
+            StackPanel panel = sender as StackPanel;
+
+            Label label = panel.Children.OfType<Label>().First();
+            label.FontWeight = FontWeights.Normal;
+
+            Rectangle rect = panel.Children.OfType<Rectangle>().First();
+            rect.Fill = Brushes.Transparent;
+        }
+
+        private void SelectPanelItem(object sender, EventArgs e)
+        {
+            StackPanel panel = sender as StackPanel;
+
+            Label label = panel.Children.OfType<Label>().First();
+
+            if (label.FontWeight == FontWeights.Bold)
+                return;
+
+            foreach (var child in this.MainPanel.Children)
+                this.DeSelectPanelItem(child);
+
+            label.FontWeight = FontWeights.Bold;
+
+            Rectangle rect = panel.Children.OfType<Rectangle>().First();
+
+            rect.Fill = Brushes.DeepSkyBlue;
+            
+        }
+
+        private void ShowGeneralPage(object sender, MouseButtonEventArgs e)
+        {
+            this.PageContent.Content = this.general;
+            this.SelectPanelItem(sender, e);
+        }
+
+        private void ShowCommandPage(object sender, MouseButtonEventArgs e)
+        {
+            this.PageContent.Content = this.command;
+            this.SelectPanelItem(sender, e);
+        }
+
+        private void ShowPowerPage(object sender, MouseButtonEventArgs e)
+        {
+            this.PageContent.Content = this.power;
+            this.SelectPanelItem(sender, e);
+        }
+
+        private void ShowPCSettingsPage(object sender, MouseButtonEventArgs e)
+        {
+            this.PageContent.Content = this.pcSettings;
+            this.SelectPanelItem(sender, e);
+        }
+    }
+}
